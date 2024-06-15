c03fec5eb901bf0531e46503efff213e2ee34564
ShaikChandini9 <chandinishaik923@gmail.com>
2024-06-15 16:00:46 -0500

    changes in app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from "./product/product.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, ProductComponent,FormsModule]
 })
 export class AppComponent {
-  title = 'ProductAngular';
+  title = '';
+  message: any;
 }
